653921d080cdb4c8e93c10f03d5ce62d65da2ac5
Marcus <marsjak16@gmail.com>
2021-05-24 21:40:43 -0500

    Made classes

---
+++ packages/backend/classes/Event.py
@@ -0,0 +1,14 @@
+import Occurence
+
+
+class Event(Occurence):
+
+    def __init__(self, name, date_time, description, tags, location):
+        super().__init__(name, date_time, description, tags)
+        self.location = location
+
+    def get_location(self) -> str:
+        return self.location
+
+    def set_location(self, new_location):
+        self.location = new_location
